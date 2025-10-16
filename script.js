f0a107cfd217a6e10eef56deed5da30bbf005d9b
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-16 17:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+{ if (arr.length <= 1) return 0;
+
+  arr.sort((a, b) => a - b);
+
+  let totalCost = 0;
+
+  while (arr.length > 1) {
+    let first = arr.shift(); 
+    let second = arr.shift();
+
+    let cost = first + second;
+    totalCost += cost;
+
+    arr.push(cost);
+    arr.sort((a, b) => a - b); 
+  }
+
+  return totalCost;
 }
 
 module.exports=mincost;
